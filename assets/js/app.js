0d94e263d742963aaedeff07256d6a4518fa2d97
JONATHAN MEYER <JONATHAN.MEYER@LIBERTYMUTUAL.COM>
2019-05-11 21:51:26 -0400

    ...

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
 (function($) {
   jQuery.fn.app = function(settings) {
     var config = { foo: "bar" };
+
     var game = $("<div>");
-    var chat = $("<div>");
+    var playersEl = $("<div>");
 
     if (settings) jQuery.extend(config, settings);
 
@@ -18,29 +19,28 @@
             .on("keyup", function(e) {
               if (e.keyCode === 13) {
                 e.preventDefault();
-                
-                console.log("set my name:", $(this).val());
-
-                $(this)
-                  .attr("readonly", true)
-                  .trigger("blur");
+                $.db.setName(
+                  $(this)
+                    .val()
+                    .trim()
+                );
               }
             })
         );
     }
 
-    function createPlayer(player) {
+    function createPlayer({ id, name }) {
+      console.log("player:", { id, name });
+
       return $("<a>")
         .addClass("list-group-item list-group-item-action player")
-        .text(player)
+        .data("player", { id, name })
+        .text(name)
         .attr("href", "#");
     }
 
     this.each(function() {
       var el = $(this);
-
-      game.addClass("card-body");
-      chat.addClass("list-group list-group-flush").append(createMe());
 
       el.addClass("app container p-1")
         .append(
@@ -56,12 +56,27 @@
                       $("<div>")
                         .addClass("card-header")
                         .text("RPS Multplayer"),
-                      game
+                      $("<div>")
+                        .addClass("card-body")
+                        .append(
+                          game
+                            .addClass("d-flex flex-row justify-content-center")
+                            .append(
+                              $("<div>").token({ image: "./assets/images/rock.png", name: "rock" }),
+                              $("<div>").token({ image: "./assets/images/paper.png", name: "paper" }),
+                              $("<div>").token({ image: "./assets/images/scissors.png", name: "scissors" })
+                            )
+                            .on("click", ".token", function(e) {
+                              console.log($(this).data("name"));
+                            }),
+                          $("<div>").append($("<span>").text("Player A: "), $("<span>").attr("id", "playerA")),
+                          $("<div>").append($("<span>").text("Player B: "), $("<span>").attr("id", "playerB"))
+                        )
                     )
                 ),
 
               $("<div>")
-                .addClass("col-3 no-gutters")
+                .addClass("col-3 col-sm-4 no-gutters")
                 .append(
                   $("<div>")
                     .addClass("card")
@@ -69,38 +84,80 @@
                       $("<div>")
                         .addClass("card-header")
                         .text("Players"),
-                      chat
-                    )
-                )
-            ),
-          $("<div>")
-            .addClass("row")
-            .append(
-              $("<div>")
-                .addClass("col-12")
-                .append(
-                  $("<form>")
-                    .addClass("form-inline ")
-                    .append(
-                      $("<div>")
-                        .addClass("input-group w-100")
-                        .append(
-                          $("<input>")
-                            .addClass("form-control mr-sm-2 my-2")
-                            .attr("name", "input"),
-                          $("<button>")
-                            .addClass("btn btn-outline-dark my-2")
-                            .text("send")
-                        )
+                      playersEl
+                        .addClass("list-group list-group-flush")
+                        .append(createMe())
+                        .on("click", ".player", function(e) {
+                          e.preventDefault();
+                          $.db.startGame($(".player-me").data("player"), $(this).data("player"));
+                        })
                     )
                 )
             )
+          // $("<div>")
+          //   .addClass("row")
+          //   .append(
+          //     $("<div>")
+          //       .addClass("col-12")
+          //       .append(
+          //         $("<form>")
+          //           .addClass("form-inline ")
+          //           .append(
+          //             $("<div>")
+          //               .addClass("input-group w-100")
+          //               .append(
+          //                 $("<input>")
+          //                   .addClass("form-control mr-sm-2 my-2")
+          //                   .attr("name", "input"),
+          //                 $("<button>")
+          //                   .addClass("btn btn-outline-dark my-2")
+          //                   .text("send")
+          //               )
+          //           )
+          //       )
+          //  )
         )
         .on("submit", "form", function(e) {
           e.preventDefault();
           console.log($(this).serializeArray());
           $(this).trigger("reset");
         });
+
+      // game.hide();
+
+      $("body").on("me.player", function(e, player) {
+        console.log("me:", player);
+
+        if (player && player.name && player.name.length > 0) {
+          $(".player-me > input").remove();
+          $(".player-me")
+            .data("player", player)
+            .text(player.name);
+        }
+      });
+
+      $("body").on("players", function(e, players) {
+        playersEl.find(".player").remove();
+        playersEl.append(players.map(player => createPlayer(player)));
+      });
+      0;
+
+      function getName({ id, name }) {
+        console.log("getName", $(".player-me").data("player"));
+
+        // if (id == $(".player-me").data("player").id) {
+        //   name += " (you)";
+        // }
+
+        return name;
+      }
+
+      $("body").on("game", function(e, game) {
+        console.log("game:", game);
+        var keys = Object.keys(game);
+        $("#playerA").text(getName({ id: keys[0], name: game[keys[0]].name }));
+        $("#playerB").text(getName({ id: keys[1], name: game[keys[1]].name }));
+      });
     });
 
     return this;
--- assets/js/database.js
+++ assets/js/database.js
@@ -1,11 +1,50 @@
-firebase.initializeApp({
-  apiKey: "AIzaSyDoizjzJgA_55Hxq879rD0mSFps-LVt47c",
-  authDomain: "rock-paper-scissors-1776.firebaseapp.com",
-  databaseURL: "https://rock-paper-scissors-1776.firebaseio.com",
-  projectId: "rock-paper-scissors-1776",
-  storageBucket: "rock-paper-scissors-1776.appspot.com",
-  messagingSenderId: "637303749509",
-  appId: "1:637303749509:web:73aabb03ad8dd251"
-});
+(function($) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyDoizjzJgA_55Hxq879rD0mSFps-LVt47c",
+    authDomain: "rock-paper-scissors-1776.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-1776.firebaseio.com",
+    projectId: "rock-paper-scissors-1776",
+    storageBucket: "rock-paper-scissors-1776.appspot.com",
+    messagingSenderId: "637303749509",
+    appId: "1:637303749509:web:73aabb03ad8dd251"
+  });
 
-var db = firebase.database();
+  var db = firebase.database();
+  var players = db.ref("players");
+  var game = db.ref("game");
+  var me = null;
+
+  $.uuid().then(uuid => {
+    me = players.child(uuid);
+
+    me.child("name").on("value", function(snap) {
+      $("body").trigger("me.player", { id: uuid, name: snap.val() });
+    });
+
+    players.on("value", function(snap) {
+      if (snap.val()) {
+        $("body").trigger("players", [
+          Object.entries(snap.toJSON())
+            .map(([key, value]) => ({ id: key, name: value.name }))
+            .filter(player => player.id != uuid)
+        ]);
+      }
+    });
+  });
+
+  game.on("value", function(snap) {
+    $("body").trigger("game", snap.val());
+  });
+
+  $.db = {
+    setName: name => {
+      me.child("name").set(name);
+    },
+    startGame: (playerA, playerB) => {
+      game.set({ [playerA.id]: { name: playerA.name }, [playerB.id]: { name: playerB.name } });
+    },
+    play: (player, play) => {
+      game.child(player.id).set({ play });
+    }
+  };
+})(jQuery);
